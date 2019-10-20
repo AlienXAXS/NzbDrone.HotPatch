8ce823c6b7254a2bba8d765894c010395d8036e6
AlienX <mark.phillips@axsemail.co.uk>
2019-10-20 18:34:53 +0100

    Our own bootstrapper loads Radarr
    We can use reflection to replace things now, without the need of injection.

---
+++ NzbDrone.HotPatch/ConsoleAlerts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Radarr.Host;
+
+namespace NzbDrone.HotPatch
+{
+    public class ConsoleAlerts : IUserAlert
+    {
+        public void Alert(string message)
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine(message);
+            System.Console.WriteLine("Press enter to continue");
+            System.Console.ReadLine();
+        }
+    }
+}
--- NzbDrone.HotPatch/Program.cs
+++ NzbDrone.HotPatch/Program.cs
@@ -1,14 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using NLog;
+using NzbDrone.Common.EnvironmentInfo;
+using NzbDrone.Common.Instrumentation;
+using Radarr.Host;
 
 namespace NzbDrone.HotPatch
 {
-    class Program
+    public static class Program
     {
+        private static readonly Logger Logger = NzbDroneLogger.GetLogger(typeof(Program));
+
+        private enum ExitCodes : int
+        {
+            Normal = 0,
+            UnknownFailure = 1,
+            RecoverableFailure = 2
+        }
+
         static void Main(string[] args)
         {
+            try
+            {
+
+                var oldColor = Console.ForegroundColor;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("AGN Gaming Radarr Hotpatch Loading...");
+                Console.WriteLine("Attempting to bootstrap Radarr, here goes!");
+
+                Console.ForegroundColor = oldColor;
+
+                var startupArgs = new StartupContext(args);
+                try
+                {
+                    NzbDroneLogger.Register(startupArgs, false, true);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine("NLog Exception: " + ex.ToString());
+                    throw;
+                }
+                Bootstrap.Start(startupArgs, new ConsoleAlerts());
+            }
+            catch (SocketException e)
+            {
+                System.Console.WriteLine("");
+                System.Console.WriteLine("");
+                Logger.Fatal(e.Message + ". This can happen if another instance of Radarr is already running another application is using the same port (default: 7878) or the user has insufficient permissions");
+                Exit(ExitCodes.RecoverableFailure);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("");
+                System.Console.WriteLine("");
+                Logger.Fatal(e, "EPIC FAIL!");
+                Exit(ExitCodes.UnknownFailure);
+            }
+        }
+
+        private static void Exit(ExitCodes exitCode)
+        {
+            LogManager.Shutdown();
+
+            if (exitCode != ExitCodes.Normal)
+            {
+                System.Console.WriteLine("Press enter to exit...");
+
+                System.Threading.Thread.Sleep(1000);
+
+                // Please note that ReadLine silently succeeds if there is no console, KeyAvailable does not.
+                System.Console.ReadLine();
+            }
+
+            Environment.Exit((int)exitCode);
         }
     }
 }
